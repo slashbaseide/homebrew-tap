e4ecb4b5771a1e010908c0eabb4977cc1b1656ad
Paras Waykole <paraswaykole12@gmail.com>
2023-04-09 17:15:38 +0530

    Update slashbase.rb

--- Casks/slashbase.rb
+++ Casks/slashbase.rb
@@ -1,6 +1,6 @@
 cask "slashbase" do
   arch arm: "mac_apple_chip", intel: "mac_intel_chip"
-  version "v0.8.3"
+  version "v0.8.4"
   sha256 :no_check
   
   url  "https://github.com/slashbaseide/slashbase/releases/download/#{version}/#{arch}.dmg"
