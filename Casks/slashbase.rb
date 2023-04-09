b02ce2b7f843b5e79e62c7474b33d8d2bf554f12
Paras Waykole <paraswaykole12@gmail.com>
2023-04-09 13:15:36 +0530

    updated cask

--- Casks/slashbase.rb
+++ Casks/slashbase.rb
@@ -1,18 +1,16 @@
 cask "Slashbase" do
+  arch arm: "mac_apple_chip", intel: "mac_intel_chip"
   version "v0.8.3"
-  sha256 arm:   "b497381ac3ed04f1438a6d64dede18d906ec41d7f63563a063db6f2ec80eeb46",
-         intel: "2de77de95422e043cce4bc9bac6c194d1f178927b632da614e0b52120c150475"
-  url    arm:   "https://github.com/slashbaseide/slashbase/releases/download/#{version}/mac_apple_chip.dmg",
-         intel: "https://github.com/slashbaseide/slashbase/releases/download/#{version}/mac_intel_chip.dmg"
+  sha256 :no_check
   
+  url  "https://github.com/slashbaseide/slashbase/releases/download/#{version}/#{arch}.dmg"
   name "Slashbase"
   desc "Open-source Modern database IDE for dev/data workflows"
   homepage "https://slashbase.com/"
   license "Apache-2.0"
   
   livecheck do
-    url  arm:   "https://github.com/slashbaseide/slashbase/releases/latest/download/mac_apple_chip.dmg",
-         intel:  "https://github.com/slashbaseide/slashbase/releases/latest/download/mac_intel_chip.dmg"
+    url "https://github.com/slashbaseide/slashbase/releases/latest/download/#{arch}.dmg"
     strategy :extract_plist
   end
 
